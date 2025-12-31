e0e968640d1459e9634aba1d433509d439f420d8
Aidan-Gildea <apgildea@gmail.com>
2025-12-30 21:04:01 -0800

    Updated comments and refined code, cut repetitive or unnecessary comments

--- include/motors.h
+++ include/motors.h
@@ -7,7 +7,11 @@
 #define SERVOMAX  560  // Maximum pulse length (out of 4096)
 #define SERVO_FREQ 50  // Servo frequency (50Hz for most analog servos)
 
-#define SERVO_ANGLE_OFFSET 60
+#define SERVO_ANGLE_OFFSET 60 // note that the servos home to 150 degrees for more ROM
+
+// should probably go in .README
+// when plugging in feedback pins, make sure they are in order A0, A1, A2, etc.
+
 
 int NUM_SERVOS;
 Adafruit_PWMServoDriver pwm;
@@ -15,40 +19,13 @@ Adafruit_PWMServoDriver pwm;
 void setupMotors(int servoAmount)
 {
     pwm = Adafruit_PWMServoDriver();
-    pwm.begin(); //ABSOLUTELY NECESSARY, DO NOT FORGET NEXT TIME
+    pwm.begin(); // very important to call this function!!
     pwm.setPWMFreq(SERVO_FREQ);
-    NUM_SERVOS = servoAmount;
 
-    if(servoAmount > 0)
-    {
-      pinMode(A0, INPUT);
-      if(servoAmount > 1)
-      {
-        pinMode(A1, INPUT);
-        if(servoAmount > 2)
-        {
-          pinMode(A2, INPUT);
-          if(servoAmount > 3)
-          {
-            pinMode(A3, INPUT);
-            if(servoAmount > 4)
-            {
-              pinMode(A4, INPUT);
-              if(servoAmount > 5)
-              {
-                pinMode(A5, INPUT);
-                if(servoAmount > 6)
-                {
-                  Serial.println("Too many servos, used all analog pins");
-                }
-              }
-            }
-          }
-        }
-      }
+    // setup for feedback pins
+    for (int i = 0; i < servoAmount && i < 6; i++) {
+        pinMode(A0 + i, INPUT); // A0, A1, etc. are sequential constants
     }
-    
-    delay(100);
 }
 
 //later on I want to improve this function by taking in an angle from the rotary encoder and using it to find how to time to slow down speed. 
@@ -59,54 +36,23 @@ bool ServosSetAngles(int args[])
   int angle2 = args[1];
   int angle3 = args[2];
 
-  Serial.println("Setting servos to angles: " + String(angle1) + ", " + String(angle2) + ", " + String(angle3));
+  // optional message for debugging
+  // Serial.println("Setting servos to angles: " + String(angle1) + ", " + String(angle2) + ", " + String(angle3));
 
   if (angle1 < 0 || angle1 > 180 || angle2 < 0 || angle2 > 180 || angle3 < 0 || angle3 > 180) {
-    Serial.println("Invalid angle(s)");
+    // optional message for debugging
+    // Serial.println("Invalid angle(s)");
     return false;
   }
 
-  pwm.setPWM(0, 0, map(angle1, 0, 180, SERVOMIN, SERVOMAX));
+  pwm.setPWM(0, 0, map(angle1, 0, 180, SERVOMIN, SERVOMAX)); 
   pwm.setPWM(1, 0, map(angle2, 0, 180, SERVOMIN, SERVOMAX));
   pwm.setPWM(2, 0, map(angle3, 0, 180, SERVOMIN, SERVOMAX));
 
   return true;
 }
 
-// bool ServosSetPath(String args)
-// {
-//   args.trim();
-//   Serial.println("Received path:");
-//   Serial.println(args);
 
-//   while (args.length() > 0) {
-//     int spacer1 = args.indexOf(' ');
-//     if (spacer1 == -1) return false;
-
-//     int spacer2 = args.indexOf(' ', spacer1 + 1);
-//     if (spacer2 == -1) return false;
-
-//     int spacer3 = args.indexOf(' ', spacer2 + 1);
-
-//     String firstString;
-//     if (spacer3 == -1) {
-//       // Last group of 3, take the rest of the string
-//       firstString = args;
-//       args = "";
-//     } else {
-//       firstString = args.substring(0, spacer3);
-//       args = args.substring(spacer3 + 1);
-//     }
-
-//     firstString.trim();
-//     Serial.print("Setting angles: ");
-//     Serial.println(firstString);
-//     ServosSetAngles(firstString);
-//     delay(250); // optional delay between movements
-//   }
-
-//   Serial.println("Path complete");
-//   return true;
 // }
 
 bool ServoSetAngle(String args)
@@ -137,13 +83,17 @@ bool ServoSetAngle(String args)
 
 void HomeServos()
 {
-  byte SERVOOFFSET = (SERVOMIN - SERVOMAX) / 3; //60 degrees offset for homing
-  Serial.println("Homing all servos to 90 degrees");
-  int degrees = (SERVOMAX - SERVOMIN) / 2 + SERVOMIN;
+  int SERVOOFFSET = (SERVOMAX - SERVOMIN) / (3); //60 degrees offset for homing
+  
+  // optional message for debugging
+  Serial.println("Homing all servos to straight position. ");
+  
+  int degrees = (SERVOMAX - SERVOMIN) / 2 + SERVOMIN; // 90 degrees
+
   pwm.setPWM(0, 0, degrees);
-  for(int i = 1; i < 3; i++)
+  for(int i = 1; i < NUM_SERVOS; i++)
   {
-    pwm.setPWM(i, 0, degrees + SERVOOFFSET);
+    pwm.setPWM(i, 0, degrees - SERVOOFFSET);
     delay(100);
   }
   
@@ -153,20 +103,9 @@ float PrintServoAngle(String index) ///always returns integer angle, I wonder wh
 {
   Serial.println("Angle is: ");
   int servoIndex = index.toInt();
-  switch(servoIndex) //I know this is trash but it works whatever
-  {
-    case 0: return(map(analogRead(A0), 104, 662, 0, 18000)/100);
-    
-    case 1: return(map(analogRead(A1), 102, 664, 0, 18000)/100);
-    
-    case 2: return(map(analogRead(A2), 104, 662, 0, 18000)/100);
-    
-    case 3: return(map(analogRead(A3), 104, 662, 0, 18000)/100);
 
-    case 4: return(map(analogRead(A4), 104, 662, 0, 18000)/100);
-    
-    case 5: return(map(analogRead(A5), 104, 662, 0, 18000)/100);
+  if(servoIndex >= NUM_SERVOS || servoIndex < 0) return -1;
 
-    default: return -1;
-  }
+  return (map(analogRead(A0+servoIndex), 104, 662, 0, 18000)/100.00);
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <communication.h>
+#include <motors.h>
 
 void setup() {
+
   //beginning serial on Baud 9600
   Serial.begin(9600);
 
-  //motors setupfor 1 servo
+  //motors setup for 3 servo
   setupMotors(3);
+
   delay(500);
-  // HelpCommand();
-  pinMode(A0, INPUT);
-  HomeServos();
+  
+  // home servos on startup - not needed every time
+  //HomeServos();
 }
 
-//testing testing
 
-const byte MAX_LEN = 15;
-char inputLine[MAX_LEN];
-int angles[3];
+const byte MAX_LEN = 15; // max length in bytes of an input line
+char inputLine[MAX_LEN]; // char array to hold the current input line
+int angles[3]; // array to hold parsed angles
 
-void loop() 
+void loop() // serial reader
 {
   static byte currIndex = 0;
   while(Serial.available() > 0)
@@ -48,16 +50,21 @@ void loop()
           ptr += strlen(ptr) + 1;  // jump past current token and null terminator
       }
 
-      Serial.print("Angles: ");
-      for (int i = 0; i < 3; i++) {
-          Serial.print(angles[i]);
-          if (i < 2) Serial.print(", ");
-      }
+      // !!!! do not want to have the IK and this robot both printing onto serial at the same time
+      // Serial.print("Angles: ");
+      // for (int i = 0; i < 3; i++) {
+      //     Serial.print(angles[i]);
+      //     if (i < 2) Serial.print(", ");
+      // }
+
+
       ServosSetAngles(angles);
+
       for(int i = 0; i < 3; i++)
       {
         angles[i] = 0; // Reset angles after processing
       }
+      
       currIndex = 0; // Reset index for next input
     }
   }
